e1d598aac02e19ca617e91202f7c60a167ac1fec
neysistemas100 <neysistemas10@gmail.com>
2023-03-30 12:43:12 -0500

    update

--- movement/src/main/java/com/nttdata/movement/AsociationApplication.java
+++ movement/src/main/java/com/nttdata/movement/AsociationApplication.java
@@ -3,12 +3,14 @@ package com.nttdata.movement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactivefeign.spring.config.EnableReactiveFeignClients;
 
 @SpringBootApplication
 @EnableReactiveFeignClients
+@EnableEurekaClient
 public class AsociationApplication {
 
 	public static void main(String[] args) {
